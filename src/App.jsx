704e314a478beeefc0f8ba6def4db46727c97200
Cailum Leyshon <cailumleyshon@gmail.com>
2023-10-25 23:57:46 +0100

    modify data and add useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import './App.css'
-import Header from "./Header"
-import Footer from "./Footer"
-import Gallery from "./Gallery"
+import Header from './Header'
+import Footer from './Footer'
+import Gallery from './Gallery'
+import data from './assets/data.json'
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Gallery />
+      <Gallery data={data}/>
       <Footer />
     </div>
   );
